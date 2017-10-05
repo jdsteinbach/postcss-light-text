3cdff733096ee28c126147f3ce1b585f75ca6a0c
James Steinbach <james.steinbach@dockyard.com>
2017-10-05 15:17:23 -0600

    tests set up & running

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const postcss = require('postcss');
 const tinycolor = require('tinycolor2');
 
 module.exports = postcss.plugin('postcss-light-text', opts => {
-  const {luminance} = {luminance: .5, ...opts};
+  const { luminance } = { luminance: 0.5, ...opts };
   return root => {
     root.walkRules(rule => {
       rule.walkDecls('color', decl => {
-        console.log(tinycolor(decl.value).getLuminance());
         if (tinycolor(decl.value).getLuminance() >= parseFloat(luminance)) {
-          rule.append({prop: '-webkit-font-smoothing', value: 'antialiased'});
+          rule.append({ prop: '-webkit-font-smoothing', value: 'antialiased' });
         }
       });
     });
--- index.test.js
+++ index.test.js
@@ -3,17 +3,23 @@ var postcss = require('postcss');
 var plugin = require('./');
 
 function run(input, output, opts) {
-    return postcss([ plugin(opts) ]).process(input)
-        .then(result => {
-            expect(result.css).toEqual(output);
-            expect(result.warnings().length).toBe(0);
-        });
+  return postcss([ plugin(opts) ]).process(input)
+    .then(result => {
+      expect(result.css).toEqual(output);
+      expect(result.warnings().length).toBe(0);
+    });
 }
 
-/* Write tests here
-
-it('does something', () => {
-    return run('a{ }', 'a{ }', { });
+test('change: light color', () => {
+  return run(
+    'a{color:#eee}',
+    'a{color:#eee;-webkit-font-smoothing:antialiased}'
+  );
 });
 
-*/
+test('no change: dark color', () => {
+  return run(
+    'a{color:#222}',
+    'a{color:#222}'
+  );
+});
